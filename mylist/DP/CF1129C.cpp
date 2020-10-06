374df7128e151a5dc6a5518b4817d6f7c731d308
1998factorial <edward19980505@gmail.com>
2020-10-06 09:55:17 +0800

    added new problem

--- mylist/DP/CF1129C.cpp
+++ mylist/DP/CF1129C.cpp
@@ -1,65 +1,66 @@
 /*
-    DP[l][r] = ans for s[l..r]
-    ans for i is sum{sum{DP[y][x] , 1 <= y <= x} , 1 <= x <= i}
-    minus the overcounted part , to avoid overcount, we let Min x to be the 
-    the longest common suffix of s[...i] and some s[...k] , k <= i
+    let DP[l][r] = the number of decoding way for s[l .. r]
+    LCS[i][j] = longest common suffix for s[...i] and s[...j]
+    ans for s[..i] is the ans for s[1...i-1] + some DP[x][i]
+    where s[x..i] has not been counted before, so x < i - max(LCS[j][i]) where j < i
 */
 #include <bits/stdc++.h>
-#define ID if(1)
-#define ll long long 
+#define MOD 1000000007
 using namespace std;
 
-const ll MOD = 1e9 + 7;
-bool mask[16];
 int a[3005];
+int DP[3005][3005];
 int LCS[3005][3005];
-ll DP[3005][3005];
 int N;
+bool mask[16];
+
+int add(int x , int y){
+    x += y;
+    if(x >= MOD)x -= MOD;
+    return x;
+}
 
 int main(){
+    mask[3] = mask[5] = mask[14] = mask[15] = 1;
     int i , j , k;
     scanf("%d" , &N);
-    mask[3] = mask[5] = mask[14] = mask[15] = 1;
-    for(i = 1; i <= N; ++i)scanf("%d" , &a[i]);
     for(i = 1; i <= N; ++i){
-        for(j = 1; j <= i; ++j){
-            if(a[i] == a[j]){
-                LCS[i][j] = 1 + LCS[i - 1][j - 1];
-                printf("LCS[%d][%d] = %d\n" , i , j , LCS[i][j]);
-            }
+        scanf("%d" , &a[i]);
+    }
+    for(i = 1; i <= N; ++i){
+        for(j = i; j >= 1; --j){
+            if(a[i] == a[j])LCS[i][j] = LCS[i - 1][j - 1] + 1;
         }
     }
     for(i = 1; i <= N; ++i)DP[i][i - 1] = 1;
     for(i = 1; i <= N; ++i){
         for(j = i; j <= N; ++j){
-            if(i == j)DP[i][j] = 1;
-            else if(i == j - 1)DP[i][j] = 2;
-            else if(i == j - 2)DP[i][j] = 4;
+            if(j - i + 1 <= 3)DP[i][j] = (1 << (j - i));
             else{
-                ll val = 0;
-                int mk;
-                for(k = 1; k <= 3; ++k)val += DP[i][j - k] , val %= MOD;
-                for(k = 0; k < 3; ++k)mk += (a[j - k] << k);
-                if(!mask[mk])val += DP[i][j - 4] , val %= MOD;
+                int val = 0;
+                for(k = 1; k <= 3; ++k){
+                    val = add(val , DP[i][j - k]); // right point use length k word
+                }
+                int vmask = 0;
+                for(k = 3; k >= 0; --k){
+                    vmask = (vmask << 1) | a[j - k]; // see if right most length 4 word is bad
+                }
+                if(!mask[vmask]){
+                    val = add(val , DP[i][j - 4]); // if not bad, we can use it
+                }
                 DP[i][j] = val;
             }
-            ID printf("DP[%d][%d] = %lld\n" , i , j , DP[i][j]);
         }
     }
-    ll ret = 0;
+    int ret = 0;
     for(i = 1; i <= N; ++i){
         int max_lcs = 0;
-        for(j = 1; j < i; ++j){
-            max_lcs = max(max_lcs , LCS[i][j]);
+        for(j = i - 1; j >= 0; --j){
+            max_lcs = max(max_lcs , LCS[i][j] + 1);
         }
-        ID cout << "max_lcs = " << max_lcs << endl;
-        ID printf("solving for i = %d we start from %d\n" , i , i - max_lcs + 1);
-        for(j = i - max_lcs + 1; j <= i + 1; ++j){
-            ret += DP[j][i]; 
-            ret %= MOD;
+        for(j = max_lcs; j <= i; ++j){
+            ret = add(ret , DP[i - j + 1][i]);
         }
-        printf("%lld\n" , ret);
+        printf("%d\n" , ret);
     }
 }
-
-
