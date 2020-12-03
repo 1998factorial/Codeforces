cc7b6e0e87b4c64984e90bc85e11e63e6713df88
1998factorial <edward19980505@gmail.com>
2020-12-03 23:09:11 +0800

    solved 908G

--- practice/DP/CF908G.cpp
+++ practice/DP/CF908G.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define rep(i , a , b) for(int i = a; i <= b; ++i)
 using namespace std;
 // Hard DP , 2800
 // S(k) = number that represents k, but digits are sorted
@@ -7,11 +8,53 @@ using namespace std;
 // X <= 1e700
 typedef long long ll;
 const ll MOD = 1e9 + 7;
-char X[705];
+char a[705];
+int X[705];
 int N;
-
+ll DP[705][705][10][2];
+// 111222333 = 111111111 + 111111 + 111
+// 1234 = 1111 + 111 + 11 + 1
+// 1...1 #(digits >= 9) + 1...1 #(digits >= 8) + .... + 1...1 #(digits >= 1)
+// for each single number
+// By this observation, it make sense to calculate the total sum by spliting to 
+// (total contribution by digits >= 9) + (total contribution by digits >= 8) + ...
+// if we fix digit d , the goal is to find the number of ways to make 
+// valid number that have j digits >= d 
+// DP[i][j][d][k] = # of ways to fill the first i digits 
+// such that there are j digits >= d, k => same , !k => strictly less
+// ans will be (DP[N][i][d][0] + DP[N][i][d][1]) * 11...11 (i ones)
+// for all i <= N , d <= 9
 int main(){
-    scanf("%s" , X);
-    N = strlen(X);
-    // TODO
+    scanf("%s" , a + 1);
+    N = strlen(a + 1);
+    rep(i , 1 , N)X[i] = a[i] - '0';
+    rep(i , 0 , 9)DP[0][0][i][1] = 1; // base
+    rep(d , 0 , 9){ // compute number of good numbers that has some number of digits >= d
+        rep(i , 0 , N - 1){
+            rep(j , 0 , i){
+                rep(k , 0 , 1){
+                    ll v = DP[i][j][d][k];
+                    if(!v)continue;
+                    rep(nd , 0 , 9){
+                        if(k && nd > X[i + 1])continue;
+                        int nk = k && nd == X[i + 1];
+                        ll& nv = DP[i + 1][j + (nd >= d)][d][nk];
+                        nv += v;
+                        if(nv >= MOD)nv -= MOD;
+                    }
+                }
+            }
+        }
+    }
+    ll ret = 0;
+    rep(d , 1 , 9){
+        ll ones = 1;
+        rep(i , 1 , N){ 
+            ll v = (DP[N][i][d][0] + DP[N][i][d][1]) % MOD;
+            ret += v * ones % MOD;
+            if(ret >= MOD)ret -= MOD;
+            ones = (ones * 10 % MOD + 1) % MOD;
+        }
+    }
+    printf("%lld\n" , ret);
 }
